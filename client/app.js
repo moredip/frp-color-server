41c7e57c1cde728663affeaf66680cce7cff7ed4
Pete Hodgson <git@thepete.net>
2016-02-08 17:26:29 -0800

    wire up a slider to send numbers to the server

--- client/app.js
+++ client/app.js
@@ -1,9 +1,24 @@
-
-
 (function(){
+  var POST_THROTTLE_IN_MS = 200;
+
   if( !Cookies.get('CLIENT_UID') ){
     Cookies.set('CLIENT_UID',uuid(),{ expires: Infinity });
   }
 
-  $.post('/sendNumber', {number:1003});
+  var $slider = $('.slider input'),
+        $label = $('.slider .label');
+
+  function valueFromEvent(e){
+    return e.target.value;
+  }
+
+  var numbers = Rx.Observable.fromEvent($slider,'input',valueFromEvent)
+    .map( parseFloat )
+    .map( function(v){ return Math.round(1000*v); } )
+
+  numbers.subscribe( function(n){ $label.text(n) } );
+
+  numbers
+    .throttleTime(POST_THROTTLE_IN_MS)
+    .subscribe( function(n){ $.post('/sendNumber',{number:n}) } );
 }());
